c705d72d6d21d223d7cb0978b9c2ca4fe9522421
DinoMondino <128713373+DinoMondino@users.noreply.github.com>
2023-03-31 12:08:49 -0300

    Update funciones2.py

--- funciones2.py
+++ funciones2.py
@@ -5,26 +5,33 @@ def opcion1():
     archivo=open("frases_de_peliculas.txt", encoding="utf-8")
     lista=archivo.readlines()
     lista=sorted(lista)
-    matriz=[]
+    lista2=[]
     for x in range(len(lista)):
-        if lista[x] == lista[x+1]:
-            lista.pop(x)
-    for x in range(len(lista)):
-        matriz.append(lista[x].split(';'))   
-        print(str(x)+")",matriz[x][1])
-        
+        y,z = lista[x].split(';')
+        lista2.append(z)
+#creo una lista con los titulos sin repetir (Usando un set(elementos unicos))
+    lista2 = list(set(lista2))
+    for x in range(len(lista2)):
+        print(str(x)+")",lista2[x])
 def opcion2():
     import random
     archivo=open("frases_de_peliculas.txt", encoding="utf-8")
     lista=archivo.readlines()
     lista=sorted(lista)
+#Se crea una matriz con [frase][nom pelicula] y la lista de nombres de peliclas
     matriz=[]
+    lista2 =[]
     for x in range(len(lista)):
-        matriz.append(lista[x].split(';'))   
+        y,z = lista[x].split(';')
+        matriz.append(lista[x].split(';'))
+        lista2.append(z) 
+    lista2 = list(set(lista2))
+#Se escoge una frase random y se muestra
     indice_f=random.randint(0,55)
     print(matriz[indice_f][0])
     print("A que pelicula pertence esta frase?")
-    lista= [matriz[indice_f][1],matriz[random.randint(0,55)][1],matriz[random.randint(0,55)][1]]
+#utilizar la lista2 baja las posibilidades de que se repita la misma pelicula en las opciones
+    lista= [matriz[indice_f][1],lista2[random.randint(0,53)],lista2[random.randint(0,53)]]
     random.shuffle(lista)
     print("1)",lista[0])
     print("2)",lista[1])
