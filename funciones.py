ebf2748f2e918ff3738ab41a6cc3d5c2e92e537e
DinoMondino <128713373+DinoMondino@users.noreply.github.com>
2023-04-01 16:02:37 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,4 +1,8 @@
 import operator
+from sympy import *
+import matplotlib.pyplot as plt
+from sympy.plotting import plot
+
 def lector(archivo_str):
   #Se abre el archivo y se extraen los datos en una lista
   archivo=open(archivo_str+".csv")
@@ -59,3 +63,36 @@ def porcentaje(archivo_str):
     resistencia=sorted(matriz_60, reverse=True)[0]
     soporte=sorted(matriz_60)[0]
     return matriz, soporte, resistencia
+  
+  def grafica(archivo_str):
+        matriz=normalizar(archivo_str)
+        coor_x=[]
+        coor_y=[]
+        contador=0
+        for x in range(len(matriz)):
+            contador+=1
+            coor_x.append(float(contador))
+            coor_y.append(float(matriz[x][1]))
+        plt.plot(coor_x,coor_y, color = "green")
+        plt.show()
+def superposicion():
+        matriz_n=normalizar("nike")
+        matriz_g=normalizar("google")
+        coor_x_n=[]
+        coor_y_n=[]
+        coor_x_g=[]
+        coor_y_g=[]
+        contador=0
+        for x in range(len(matriz_n)):
+            contador+=1
+            coor_x_n.append(float(contador))
+            coor_y_n.append(float(matriz_n[x][1]))
+            coor_x_g.append(float(contador))
+            coor_y_g.append(float(matriz_g[x][1]))
+
+        plt.plot(coor_x_n,coor_y_n,color='purple', label="purple")
+        plt.plot(coor_x_g,coor_y_g,color='orange', label="orange")
+        plt.title("Precios normalizados de 0 a 1")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
